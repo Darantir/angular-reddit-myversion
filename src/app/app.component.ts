bd8f4430df484cf9513a85fb5b1966f41552794f
Darantir <yakushev_1996@inbox.ru>
2020-04-06 21:30:50 +0200

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article} from './article/article.module';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
+  articles: Article[];
+
+  constructor(){
+    this.articles = [
+      new Article('Angular 2', 'http://angular.io',3),
+      new Article('FullStack', 'http://fullstack.io',2),
+      new Article('Angular Homepage', 'http://angular.io',1),
+    ]
+  }
   AddArticle(title: HTMLInputElement, link: HTMLInputElement): boolean
   {
     console.log('Adding article title: ${title.value} and link: ${link.value}');
